abb37d1e0ec03e54c7dfacd398779e05762e1b61
sihgo109 <89370531+sihgo109@users.noreply.github.com>
2021-09-10 11:30:16 +0900

    不要ファイル削除

--- app/controllers/users_controller_bk.rb
+++
@@ -1,7 +0,0 @@
-class UsersController < ApplicationController
-  def show
-    @user = User.find(params[:id])
-    @pusername = current_user.username
-    @prototypes = current_user.prototypes
-  end
-end
